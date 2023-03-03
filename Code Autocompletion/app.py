fcf9f6d7c1faf9aa659b817a219f8224ea9f1dcb
Rambo_SORN <104191123+rambosorn@users.noreply.github.com>
2023-03-03 13:24:11 +0700

    Add files via upload

---
+++ Code Autocompletion/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify, render_template
+import openai
+import os
+
+openai.api_key = "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"
+
+app = Flask(__name__, static_url_path='/static')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/autocomplete', methods=['POST'])
+def autocomplete():
+    data = request.get_json()
+    input_code = data['code']
+
+    model_engine = "davinci-codex"
+    prompt = (f"Given the following Python code, complete the code:\n\n{input_code}\n\nCode:")
+    try:
+        completions = openai.Completion.create(engine=model_engine, prompt=prompt, max_tokens=150, n=1,stop=None,temperature=0.7)
+        message = completions.choices[0].text
+        return jsonify({'suggestions': [message]})
+    except Exception as e:
+        print(e)
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
